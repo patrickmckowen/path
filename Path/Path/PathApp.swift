44ee9a320961fe2cb1290edb580a027fb54b60e8
patrickmckowen <pmckowen@hey.com>
2020-11-03 10:09:30 -0800

    Creating xcode project

---
+++ Path/Path/PathApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathApp.swift
+//  Path
+//
+//  Created by Patrick McKowen on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct PathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
